d07ce0da8c93ebfd29765f7fbdb0b491c037379b
Prokop Svaćina <prokop.svacina@gmail.com>
2020-11-10 18:54:26 +0100

    Guys I made the main!!! GUYS! I MADE THE 0.2 version!!!

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	//ADDED MAIN GUYS!!!! WORKS IN Visual Basic IDE, it doesnt run yet but shows no error guys! I just need to make it run!!!
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		run console
+		
+		open consolw window
+		and hack mineplex minecraft servr;
+		
+		end program and do a csharp versinvvvvvvvvvvvvvvvvvvvv
+		
+	}
+
+}
